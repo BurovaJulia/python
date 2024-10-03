8c7cf3536336fdeacc10bd0f6bd921a73c6af5b3
BurovaJulia <143869261+BurovaJulia@users.noreply.github.com>
2024-10-03 08:43:18 +0300

    Update алгосы лаба1 задание1.py

--- алгосы лаба1 задание1.py
+++ алгосы лаба1 задание1.py
@@ -1,11 +1,12 @@
 #Задание №1
 
+import time
 import numpy as np
-import random
+import matplotlib.pyplot as plt
 
-vec = np.random.randint(0, (10**5)*17, (10**5)*17)  # создали огромный вектор со случ значениями
+vec = np.random.randint(1, (10**4)*3, (10**4)*3)  # создали огромный вектор со случ значениями
 #vec = np.random.randint(1, 11, 10)  # нижний диапазон вкл, верх диапазон не вкл, длина
-print(vec, end='\n\n')
+#print(vec, end='\n\n')
 
 def sum_of_nums(vector):
     sum_v = 0
@@ -36,7 +37,69 @@ def mid_harmonic(vector):
         sum_vk += 1/vector[i]
     return sum_vk
 
-for n in range(1, (10**5)*17, 1700):
-#for n in range(1, 11, 2):
-    v = vec[0:n]  # создаем мини-векторы для каждого случая
-    print(sum_of_nums(v), search_of_max(v), search_of_min(v), n/mid_harmonic(v))
+def average_t(times):
+    return sum(times) / len(times)
+
+def build_graph(time, range_):
+    plt.plot(range_, time)
+    plt.xlabel('Размер массива, n')
+    plt.ylabel('Среднее время выполнения, sec')
+    plt.title('Зависимость времени от размера массива')
+    plt.show()
+    plt.grid(True)
+
+def time_measurement():
+    execution_time_sum = []  # список времени выполнения
+    execution_time_max = []
+    execution_time_min = []
+    execution_time_harmonic = []
+    n_values = []  # список значений при которых выполняются измерения
+
+    for n in range(1, (10 ** 4) * 3, 300):
+        v = vec[:n]
+        single_times_sum = []  # список 5 замеров для каждого n
+        single_times_max = []
+        single_times_min = []
+        single_times_harmonic = []
+        for _ in range(5):
+            start_time = time.time()
+            sum_of_nums(v)
+            end_time = time.time()
+            single_times_sum.append(end_time - start_time)
+
+            start_time = time.time()
+            search_of_max(v)
+            end_time = time.time()
+            single_times_max.append(end_time - start_time)
+
+            start_time = time.time()
+            search_of_min(v)
+            end_time = time.time()
+            single_times_min.append(end_time - start_time)
+
+            start_time = time.time()
+            mid_harmonic(v)
+            end_time = time.time()
+            single_times_harmonic.append(end_time - start_time)
+
+        avg_time_sum = average_t(single_times_sum)
+        execution_time_sum.append(avg_time_sum)
+
+        avg_time_max = average_t(single_times_max)
+        execution_time_max.append(avg_time_max)
+
+        avg_time_min = average_t(single_times_min)
+        execution_time_min.append(avg_time_min)
+
+        avg_time_harmonic = average_t(single_times_harmonic)
+        execution_time_harmonic.append(avg_time_harmonic)
+
+        n_values.append(n)
+
+    build_graph(execution_time_sum, n_values)
+    build_graph(execution_time_max, n_values)
+    build_graph(execution_time_min, n_values)
+    build_graph(execution_time_harmonic, n_values)
+    # return execution_time_sum, execution_time_max, execution_time_min, execution_time_harmonic, n_values
+
+time_measurement()
