1db0ef5d0a9c6ca9883378da302c0d86e0d3f750
BurovaJulia <143869261+BurovaJulia@users.noreply.github.com>
2024-10-03 08:43:54 +0300

    Update алгосы лаба1 задание2.py

--- алгосы лаба1 задание2.py
+++ алгосы лаба1 задание2.py
@@ -1,9 +1,11 @@
 # Задание 2
 
 import numpy as np
+import time
+import matplotlib.pyplot as plt
 
-matrix_A = np.random.randint(0, (10**5)*3, ((10**4)*3, (10**4)*3))  # создали случ матрицу n*n
-matrix_B = np.random.randint(0, (10**5)*3, ((10**4)*3, (10**4)*3))
+matrix_A = np.random.randint(0, (10**3)*3, ((10**2)*3, (10**2)*3))  # создали случ матрицу n*n
+matrix_B = np.random.randint(0, (10**3)*3, ((10**2)*3, (10**2)*3))
 #matrix_A = np.random.randint(0, 5, (3, 3))  # границы допустимых значений, размер матрицы
 #matrix_B = np.random.randint(0, 5, (3, 3))
 
@@ -15,10 +17,34 @@ def multiplication_of_mats(m1, m2):
                 matrix_C[i][j] += m1[i][k] * m2[k][j]
     return (matrix_C)
 
-#print(matrix_A, matrix_B, sep='\n\n', end='\n\n')
+def average_t(times):
+    return sum(times) / len(times)
 
-for n in range (1, (10**5)*17, 1700):
-#for n in range (1, 4):
-    m_A = matrix_A[:n, :n]  # от большой матрицы берем маленькие матрицы
-    m_B = matrix_B[:n, :n]
-    print(multiplication_of_mats(m_A, m_B), sep='\n\n', end='\n\n')
+def build_graph(time, range_):
+    plt.plot(range_, time)
+    plt.xlabel('Размер массива, n')
+    plt.ylabel('Среднее время выполнения, sec')
+    plt.title('Зависимость времени от размера массива')
+    plt.show()
+    plt.grid(True)
+
+def time_measurement():
+    execution_time = []  # список времени выполнения
+    n_values = []  # список значений при которых выполняются измерения
+    for n in range(1, (10 ** 2)*3, 30):
+        single_time = []  # список 5 замеров для каждого n
+        m1 = matrix_A[:n, :n]
+        m2 = matrix_B[:n, :n]
+        for _ in range(5):
+            start_time = time.time()
+            multiplication_of_mats(m1, m2)
+            end_time = time.time()
+            single_time.append(end_time - start_time)
+
+        avg_time = average_t(single_time)
+        execution_time.append(avg_time)
+        n_values.append(n)
+
+    build_graph(execution_time, n_values)
+
+time_measurement()
